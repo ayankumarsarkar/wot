addeb2c0b05991caf941a2a6f637314153f51384
Aeon <ayans5929@gmail.com>
2026-09-03 19:43:01 +0530

    Moved over to Toga for quick app building

--- src/main.py
+++ src/main.py
@@ -1,28 +1,43 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox 
+import toga
+from toga.constants import COLUMN
 
-def main_layout(root):
-    label = ttk.Label(root, text="Welcome!", font=("Ariel", 16))
-    label.pack(pady=20)
+import re
+from pathlib import Path
 
-    def handle_click():
-        user_text = entry_field.get()
-        messagebox.showinfo("Alert", f"You typed: {user_text}")
-    
-    entry_field = ttk.Entry(root)
-    entry_field.pack(pady=10)
-    
-    action_btn = ttk.Button(root, text="Submit", command=handle_click)
-    action_btn.pack()
+class WOT(toga.App):
+    def startup(self):
+        self.main_window = toga.MainWindow()
+        self.text_input = toga.MultilineTextInput()
+
+        box             = toga.Box(direction=COLUMN, margin=5)
+        paths_box       = toga.Box(direction=COLUMN, margin=5)
+        projects_box    = toga.Box(direction=COLUMN, margin=5)
+
+        active_path     = 'C:/Users/Admin/Desktop'
+        archive_path    = 'C:/Users/Admin/Documents'
+
+        topic_code_pattern = re.compile(r'^[A-Z]{3}-')
+
+        active_label    = toga.Label(text='Active Projects Path: {}'.format(active_path))
+        archive_label   = toga.Label(text='Archived Projects Path: {}'.format(archive_path))
+
+        paths_box.add(active_label)
+        paths_box.add(archive_label)
+
+        project_list    = [path.name for path in Path(archive_path).iterdir() if path.is_dir and topic_code_pattern.match(path.name)]
+
+        for project in project_list:
+            label = toga.Label(text=project)
+            projects_box.add(label)
+        
+        box.add(paths_box)
+        box.add(projects_box)
+
+        self.main_window.content = box
+        self.main_window.show()
 
 def main():
-    root = tk.Tk()
-    root.title("Workflow Organising Tool")
-    root.geometry("400x200")
-    main_layout(root)
-    print("Python project initialized successfully!")
-    root.mainloop()
+    return WOT("Worflow Organising Tool", "in.new.wot")
 
 if __name__ == "__main__":
-    main()
+    main().main_loop()
